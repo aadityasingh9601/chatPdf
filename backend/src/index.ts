9cef6cec1dcb56fc0a3c21d95ddead2ff1d563a7
Aaditya Singh <aadityasingh9601@gmail.com>
2026-07-11 06:22:34 +0000

    setup prisma postgres with supabase

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const val = await prisma.user.findMany({
+  });
+  console.log(val);
+}
+
+main();
